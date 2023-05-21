8316565d690d6a052503c893ee489d11722fcc47
Olga Akhlamova <olga-akhlamova@yandex.ru>
2023-05-21 22:50:34 +0300

    fixed errors with headers

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const { CORS } = require('./middlewares/CORS');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const { CORS } = require('./middlewares/CORS');
+const cors = require('cors');
+const { errorLogger } = require('./middlewares/logger');
+// const { requestLogger} = require('./middlewares/logger');
 const routes = require('./routes/index');
 const mainErrorHandler = require('./middlewares/mainErrorHandler');
 const apiRequestLimiter = require('./middlewares/apiRequestLimiter');
@@ -14,10 +16,14 @@ const apiRequestLimiter = require('./middlewares/apiRequestLimiter');
 const { PORT = 3700, DB_ADRESS = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const app = express();
 
-app.use(requestLogger); // подключаем логгер запросов
-app.use(apiRequestLimiter); // устанавливаем ограничение на запросы
 app.use(helmet());
-app.use(CORS);
+// app.use(requestLogger); // подключаем логгер запросов
+app.use(apiRequestLimiter); // устанавливаем ограничение на запросы
+// app.use(CORS);
+app.use(cors({
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
